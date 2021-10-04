61411c7e067d09e9a1f56413ae8fcb6459f5eb7e
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-10-03 21:37:03 -0500

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,35 @@
-var apiKey = "e0d1a433124b17c169eb4de2b1f76ba7"
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+var apiKey = "e0d1a433124b17c169eb4de2b1f76ba7"
 var searchCity = "oshkosh"
 var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey;
-var todaysDate = $('#sect-h2')
+var todaysDate = $('#sect-h2');
+var 
+
+// currentCity.text(response.name);
+// currentCity.append("<small class='text-muted' id='current-date'>");
+// $("#current-date").text("(" + currentDate + ")");
+// currentCity.append("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='" + response.weather[0].main + "' />" )
+// currentTemp.text(response.main.temp);
+// currentTemp.append("&deg;F");
+// currentHumidity.text(response.main.humidity + "%");
+// currentWindSpeed.text(response.wind.speed + "MPH");
 
 // Find current date and display in title
 var currentDate = moment().format('L');
 $("#current-date").text("(" + currentDate + ")");
 console.log(currentDate);
 
-fetch(apiUrl)
-.then(function(response) {
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  console.log(response);
-  return response.blob();
-})
-.then(function(response) {
-  let objectURL = URL.createObjectURL(response);
-  console.log(objectURL);
+$.ajax({
+    url: apiUrl,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+    currentCity.text(response.name);
+    currentCity.append("<small class='text-muted' id='current-date'>");
+    $("#current-date").text("(" + currentDate + ")");
+    currentCity.append("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='" + response.weather[0].main + "' />" )
+    currentTemp.text(response.main.temp);
+    currentTemp.append("&deg;F");
+    currentHumidity.text(response.main.humidity + "%");
+    currentWindSpeed.text(response.wind.speed + "MPH");
 });
