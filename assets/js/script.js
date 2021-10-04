9e8644f86dfa4e30ecd9e28c53f68af9a807c28b
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-10-03 20:49:18 -0500

    api call working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
-var testing = function() {
-    console.log("yo!");
-}
+var apiKey = "e0d1a433124b17c169eb4de2b1f76ba7"
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+var searchCity = "oshkosh"
+var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey;
+var todaysDate = $('#sect-h2')
 
-testing();
+// Find current date and display in title
+var currentDate = moment().format('L');
+$("#current-date").text("(" + currentDate + ")");
+console.log(currentDate);
+
+fetch(apiUrl)
+.then(function(response) {
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  console.log(response);
+  return response.blob();
+})
+.then(function(response) {
+  let objectURL = URL.createObjectURL(response);
+  console.log(objectURL);
+});
