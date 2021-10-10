276149aade9019626b9cc498cb17f694e3bbb15e
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-10-09 20:07:49 -0500

    fixed deleted code?

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,42 +7,116 @@ var currentHumidity = $('#current-humidity');
 var currentWindSpeed = $('#current-wind');
 var currentUv = $('#current-uv');
 var currentImg = $('#current-img');
-
+var pastSearches = [] 
 var searchInput
-var name;
 
-var globalThing ='first global val'
+$(window).ready(function(pastSearches) {
+    console.log('ready')
+    if (localStorage.getItem('searchHistroy')) {
+        pastSearches = JSON.parse(localStorage.getItem('searchHistroy'))
+        console.log(pastSearches.length)
+
+        for (var i = 0; i < pastSearches.length; i++) {
+            console.log('activated history for loop')
+            var html = $(`
+                    <li><button class="btn button-style city-button" type="button" id="search-city-button${i}">${pastSearches[i]}</button></li>
+            `)
+    
+            $('#search-history-list').append(html)
+        }
+    }
+})
 
 // Find current date and display in title
 var currentDate = moment().format('L');
 $("#current-date").text("(" + currentDate + ")");
-// console.log('current temp' + JSON.stringify(currentTemp));
 
+// api call with search input
 $('#search').on("click", function (event, searchInput) {
     event.preventDefault();
-
-    // searchInput = '';
     var searchInput = $('#search-city').val();
     console.log(searchInput);
-    // Grab value entered into search bar 
-    // if (!$('#search')) {
-    //     var searchInput = "neenah";
-    // } else {
-    //     var searchInput = JSON.stringify($('#search').val().trim());
-    // }
     searchHistory(searchInput, ' TEST SECOND CITY');
     getCurrentConditions(searchInput)
-   
-  
- 
+})
 
+$('#search-history-list').on("click", function (event) {
+    event.preventDefault();
+    $('.city-button').each(function() {
+        $(this).click(function(){
+            var id = "#" + $(this).attr('id') 
+            console.log(id)
+            var historyCity = $(id).text()
+            console.log(historyCity)
+
+            // searchHistory(historyCity);
+            callHistoricalConditions(historyCity)
+
+        });
+     });    
 })
 
 
+var callHistoricalConditions = function (historyCity) {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + historyCity + "&appid=" + apiKey + "&units=imperial";
+    fetch(apiUrl)
+        .then(function (data) {
+            console.log(data)
+            return data.json()
+        })
+        .then(function (response) {
+            // console.log('can we see the data ???',data);
 
+            currentCity.text(response.name);  
+            //  console.log(data.weather[0].main)  
+            $(todaysDate).text("(" + currentDate + ")")
+            $('#current-img').empty()
+            $('#current-img').append("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='" + response.weather[0].main + "' />")
+            currentTemp.text("Temp: " + response.main.temp);
+            currentTemp.append("&deg;F");
+            currentHumidity.text("Humidity: " + response.main.humidity + "%");
+            currentWindSpeed.text("Wind Speed: " + response.wind.speed + "MPH");
 
+            var latitude = response.coord.lat;
+            var longitude = response.coord.lon;
+
+            var oneCall = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=hourly,minutely&appid=' + apiKey + '&units=imperial'
+           
+            // AJAX Call for UV index
+            $.ajax({
+                url: oneCall,
+                method: "GET"
+            }).then(function (response) {
+                currentUv.text("UV Index: " + response.current.uvi);
+                console.log('Everything we need ? daily and UVI ??', response);
+                $('#five-day-forecast').empty()
+                for (let i = 0; i < 5; i++) {
+                    var someDate = new Date()
+                    console.log('activated for loop')
+                    var dd = someDate.getDate();
+                    var mm = someDate.getMonth() + i;
+                    var y = someDate.getFullYear();
+
+                    var someFormattedDate = dd + '/' + mm + '/' + y;
+                    var html = $(`
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Date ${someFormattedDate}</h5>
+                           ${"<img src='https://openweathermap.org/img/w/" + response.daily[i].weather[0].icon + ".png' alt='" + response.daily[i].weather[0].icon + "' />"}
+                            <p class="card-text">Temp: ${response.daily[i].temp.day} </p>
+                            <p class="card-text">Wind: ${response.daily[i].wind_speed}</p>
+                            <p class="card-text">Humidity: ${response.daily[i].humidity}</p>
+                        </div>
+                    </div>
+                    `)
+                    
+                    $('#five-day-forecast').append(html)
+
+                }
+            });
+        });
+}
 var getCurrentConditions = function (searchInput) {
-console.log('global thing test ??', name)
     var searchCity = searchInput;
     console.log(searchCity);
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey + "&units=imperial";
@@ -81,7 +155,7 @@ console.log('global thing test ??', name)
                 $('#five-day-forecast').empty()
                 for (let i = 0; i < 5; i++) {
                     var someDate = new Date()
-
+                    console.log('activated for loop')
                     var dd = someDate.getDate();
                     var mm = someDate.getMonth() + i;
                     var y = someDate.getFullYear();
@@ -114,13 +188,7 @@ console.log('global thing test ??', name)
 
 function searchHistory(city) {
     console.log('CITY TO SAVE', city)
-    var pastSearches = []    
-
-    if (localStorage.getItem('searchHistroy')) {
-        pastSearches = JSON.parse(localStorage.getItem('searchHistroy'))
-        console.log(pastSearches.length)
-        
-    }
+    
     //push the new city into the pastSearches array
     if (!pastSearches.includes(city) && pastSearches.length < 8 && city != "") {
         pastSearches.push(city)
@@ -148,8 +216,9 @@ function addHtmlHistory(pastSearches) {
     $('#search-history-list').empty()
 
     for (var i = 0; i < pastSearches.length; i++) {
+        console.log('activated history for loop')
         var html = $(`
-                <li><button class="btn button-style city-button" type="button" id="search-city-button">${pastSearches[i]}</button></li>
+                <li><button class="btn button-style city-button" type="button" id="search-city-button${i}">${pastSearches[i]}</button></li>
         `)
 
         $('#search-history-list').append(html)
