2f7fa6b02c594a02bd1a8b174e858590eef87f27
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-10-09 20:24:53 -0500

    fixed bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,6 @@ $('#search').on("click", function (event, searchInput) {
     searchHistory(searchInput, ' TEST SECOND CITY');
     getCurrentConditions(searchInput)
 })
-<<<<<<< HEAD
 
 $('#search-history-list').on("click", function (event) {
     event.preventDefault();
@@ -53,36 +52,10 @@ $('#search-history-list').on("click", function (event) {
             // searchHistory(historyCity);
             callHistoricalConditions(historyCity)
 
-=======
-
-$('#search-history-list').on("click", function (event) {
-    event.preventDefault();
-    $('.city-button').each(function() {
-        $(this).click(function(){
-            var id = "#" + $(this).attr('id') 
-            console.log(id)
-            var historyCity = $(id).text()
-            console.log(historyCity)
-
-            // searchHistory(historyCity);
-            callHistoricalConditions(historyCity)
-
->>>>>>> develop
         });
      });    
 })
 
-var callHistoricalConditions = function (historyCity) {
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + historyCity + "&appid=" + apiKey + "&units=imperial";
-    fetch(apiUrl)
-        .then(function (data) {
-            console.log(data)
-            return data.json()
-        })
-        .then(function (response) {
-            currentCity.text(response.name);
-
-<<<<<<< HEAD
 var callHistoricalConditions = function (historyCity) {
     console.log('callHistoricalConditions')
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + historyCity + "&appid=" + apiKey + "&units=imperial";
@@ -92,12 +65,7 @@ var callHistoricalConditions = function (historyCity) {
             return data.json()
         })
         .then(function (response) {
-            // console.log('can we see the data ???',data);
-
             currentCity.text(response.name);  
-            //  console.log(data.weather[0].main)  
-=======
->>>>>>> develop
             $(todaysDate).text("(" + currentDate + ")")
             $('#current-img').empty()
             $('#current-img').append("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='" + response.weather[0].main + "' />")
@@ -105,7 +73,6 @@ var callHistoricalConditions = function (historyCity) {
             currentTemp.append("&deg;F");
             currentHumidity.text("Humidity: " + response.main.humidity + "%");
             currentWindSpeed.text("Wind Speed: " + response.wind.speed + "MPH");
-<<<<<<< HEAD
 
             var latitude = response.coord.lat;
             var longitude = response.coord.lon;
@@ -121,7 +88,7 @@ var callHistoricalConditions = function (historyCity) {
                 var uvText = response.current.uvi
                 console.log(uvText)
                 switch(uvText) {
-                    case x < 2:
+                    case uvText < 2:
                         $(currentUv).attr('class', 'green');
                     break;
                     case 2:
@@ -154,52 +121,12 @@ var callHistoricalConditions = function (historyCity) {
                     
                     $('#five-day-forecast').append(html)
 
-=======
-
-            var latitude = response.coord.lat;
-            var longitude = response.coord.lon;
-
-            var oneCall = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=hourly,minutely&appid=' + apiKey + '&units=imperial'
-           
-            // AJAX Call for UV index
-            $.ajax({
-                url: oneCall,
-                method: "GET"
-            }).then(function (response) {
-                currentUv.text("UV Index: " + response.current.uvi);
-                console.log('Everything we need ? daily and UVI ??', response);
-                $('#five-day-forecast').empty()
-                for (let i = 0; i < 5; i++) {
-                    var someDate = new Date()
-                    console.log('activated for loop')
-                    var dd = someDate.getDate();
-                    var mm = someDate.getMonth() + i;
-                    var y = someDate.getFullYear();
-
-                    var someFormattedDate = dd + '/' + mm + '/' + y;
-                    var html = $(`
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Date ${someFormattedDate}</h5>
-                           ${"<img src='https://openweathermap.org/img/w/" + response.daily[i].weather[0].icon + ".png' alt='" + response.daily[i].weather[0].icon + "' />"}
-                            <p class="card-text">Temp: ${response.daily[i].temp.day} </p>
-                            <p class="card-text">Wind: ${response.daily[i].wind_speed}</p>
-                            <p class="card-text">Humidity: ${response.daily[i].humidity}</p>
-                        </div>
-                    </div>
-                    `)
-                    
-                    $('#five-day-forecast').append(html)
->>>>>>> develop
                 }
             });
         });
 }
 var getCurrentConditions = function (searchInput) {
-<<<<<<< HEAD
     console.log('getCurrentConditions');
-=======
->>>>>>> develop
     var searchCity = searchInput;
     console.log(searchCity);
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey + "&units=imperial";
@@ -258,12 +185,8 @@ var getCurrentConditions = function (searchInput) {
 }
 
 function searchHistory(city) {
-<<<<<<< HEAD
     console.log('CITY TO SAVE', city)
     
-=======
-    console.log('CITY TO SAVE', city)    
->>>>>>> develop
     //push the new city into the pastSearches array
     if (!pastSearches.includes(city) && pastSearches.length < 8 && city != "") {
         pastSearches.push(city)
@@ -292,3 +215,4 @@ function addHtmlHistory(pastSearches) {
         $('#search-history-list').append(html)
     }
 }
+        
